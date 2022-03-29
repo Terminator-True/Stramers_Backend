fc25a3ad8aa5f2a766a7e06d93d6557f62d741fb
Terminator_True <jf28renault@gmail.com>
2022-03-29 12:40:12 +0200

    003-joel

--- src/controllers/Carta.js
+++ src/controllers/Carta.js
@@ -38,29 +38,63 @@ var controller = {
             })
 
     },
-    saveCardt:  function( req, res ){
+    saveCard:  function( req, res ){
         var card = new Carta();
         var params = req.body;
 
-
         card.name=params.name
-        card.description=params.description
         card.category=params.category
-        card.year=params.year    
-        card.langs = params.langs
+        card.type=params.type    
+        card.coste = params.coste
+        card.daño = params.daño
+        card.vida = params.vida
+        card.text=params.text
+        card.funcion=params.funcion
         card.image = 'null'
-        
-        console.log(params)
-        project.save(projectStored)
-            .then(projectStored=>{
-                if(!projectStored) return res.status(404).send({message: "Document no desat"});
+        card.save(cardStored)
+            .then(cardStored=>{
+                if(!cardStored) return res.status(404).send({message: "Document no desat"});
 
-                return res.status(200).send({project: projectStored});
+                return res.status(200).send({project: cardStored});
             })
             .catch(err => {
                 return res.status(500).send({message: "Error desant dades"});
             })
 
+    },
+    deleteCard: function(req, res){
+        var Id = req.query.id;
+        Carta.findByIdAndDelete(Id)
+            .then(cardRemoved => {
+                if(!cardRemoved) return res.status(404).send({message:"La carta a borrar no existeix"});
+
+                return res.status(200).send({project:cardRemoved});
+            })
+            .catch(err => {
+                return res.status(500).send({message:"Error no s\'ha pogut  borrar la carta"});
+            })
+
+    },
+    uploadImage: function(req, res){
+        var Id = req.params.id;
+        var fileName = "imatge no pujada"
+
+        if(req.files){
+            var filePath = req.files.image.path
+            var filesplit=filePath.split("/")
+            var fileName=filesplit[1]
+            Project.findByIdAndUpdate(Id, {image:fileName}, {new:true})
+                .then(cardUpdated=> {
+                    if(!cardUpdated) return res.status(404).send({message:"La carta no existeix"});
+
+                    return res.status(200).send({project:cardUpdated});
+                })   
+                .catch(err =>{
+                    return res.status(500).send({message:"Error actualitzant la imatge"});
+                }) 
+        }else{
+            return res.status(500).send({message:fileName})
+        }
     }/*,
     test:  function(req, res){
         return res.status(200).send({
--- src/routes.js
+++ src/routes.js
@@ -5,13 +5,22 @@ var CardController = require("./controllers/Carta")
 
 var router = express.Router();
 
+//Paquete necesario para poder subir archivos por post
 var multipart = require("connect-multiparty");
+//Especificamos la rita de descarga
+var multiparMiddleware = multipart({uploadDir: "./src/uploads"})
 
-var multiparMiddleware = multipart({UploadDir: "./uploads"})
-
-//Rutes
-router.get("/",CardController.home)  
+//Rutas
+router.get("/",CardController.home)
+//Busca carta por id  
 router.get("/carta/:id?",CardController.getCard)  
-router.get("/cartas",CardController.getCards)  
+//Devuelve todas las cartas
+router.get("/cartas",CardController.getCards) 
+//Añade carta a la base de datos 
+router.post("/+carta",CardController.saveCard)  
+//Borra la carta pasada por parámetro
+router.delete("/cartas/:id?",CardController.deleteCard) 
+//Pujar foto de la carta
+router.post("/upload-image/:id",multiparMiddleware,CardController.uploadImage)
 
 module.exports = router;
