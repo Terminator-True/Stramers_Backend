d628eda5355483ddea041c648af9d2e1f73a2495
Terminator-True <jf28renault@gmail.com>
2022-03-31 17:34:11 +0200

    005-joel

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(bodyParser.json());
 
-//Rutes
-app.use("/api",_routes)
 //Sessió
 app.use(sessions({
     secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
@@ -25,10 +23,13 @@ app.use((req, res, next) => {
     //Fa que els recursos de la resposta puguin ser compartits per tots (*) 
     res.header('Access-Control-Allow-Origin' , '*');
     // Indica quins encapçalats HTTP poden ser utilitzats en aquesta sol·licitut
-    res.header('Access-Control-Allow-Headers' , 'Authorization, X-API-KEY,Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method, XMLHttpRequest' );
+    res.header('Access-Control-Allow-Headers' , 'Authorization, X-API-KEY,Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method' );
     //Especificació dels métodes acceptats
     res.header('Access-Control-Allow-Methods' , 'GET, POST, OPTIONS, PUT,DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE' );
     next();
    });
+
+//Rutes
+app.use("/api",_routes)
 module.exports = app;
--- src/controllers/User.js
+++ src/controllers/User.js
@@ -1,25 +1,41 @@
 'use strict'
 
 const Usuario = require("../models/User");
-const CryptoJS = require('crypto-js');
 var session={};
 
-function encryp(data, key="shjfi2857dkagr7"){
-    return CryptoJS.AES.encrypt(data, key).toString();
+//Checking the crypto module
+const crypto = require('crypto');
+const algorithm = 'aes-256-cbc'; //Using AES encryption
+const key = "Secret123";
+const iv = "sec";
+
+//Encrypting text
+function encrypt(text) {
+   let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
+   let encrypted = cipher.update(text);
+   encrypted = Buffer.concat([encrypted, cipher.final()]);
+   return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
 }
 
-function decryp(data, key="shjfi2857dkagr7"){
-    var wA= CryptoJS.AES.decrypt(data, key);
-    return wA.toString(CryptoJS.enc.Utf8);
+// Decrypting text
+function decrypt(text) {
+   let iv = Buffer.from(text.iv, 'hex');
+   let encryptedText = Buffer.from(text.encryptedData, 'hex');
+   let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
+   let decrypted = decipher.update(encryptedText);
+   decrypted = Buffer.concat([decrypted, decipher.final()]);
+   return decrypted.toString();
 }
+
 
 var controller = {
     login: function(req, res){
-        let username = req.body.username;
-        let password = req.body.password;
+        let username = req.body.email;
+        let password = req.body.pasw;
         Usuario.findOne({ email: username },'password')
             .then(pswd => {
-                if(decryp(pswd.password)!=password){
+                console.log(decrypt(pswd.password.toString(),"secret123"))
+                if(decrypt(pswd.password,"secret123")!=password){
                     return res.status(404).send({message:"Error, password incorrecte"});
                 } else{
                     session.loggedin = true;
@@ -37,11 +53,11 @@ var controller = {
 
         user.nick = params.nick
         user.email = params.email
-        user.password = encryp(params.password)    
+        user.password = encrypt(params.password,"secret123")    
         user.cartas = {}
         user.mazos = {}
         user.moneda = 0
-        user.save(userStored)
+        user.save()
             .then(userStored=>{
                 if(!userStored) return res.status(404).send({message: "Document no desat"});
 
