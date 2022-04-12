a99d6721961babf8d3b3e7b61dbe35f267f0cedd
Terminator_True <jf28renault@gmail.com>
2022-04-12 20:34:55 +0200

    015-Joel

--- app.js
+++ app.js
@@ -3,9 +3,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var _routes = require("./src/routes")
-const http = require("http").createServer(express);
 const cors = require("cors");
-const io = require("socket.io")(http);
+
 var app = express();
 
 app.use(bodyParser.urlencoded({extended:false}));
